627e8be7961f6e45183bb551d7bf9383ab47dccd
Joey Robinson <theamazingness@gmail.com>
2019-04-02 16:45:05 -0400

    express-boilerplate

---
+++ server.js
@@ -0,0 +1,24 @@
+// Importing .env file which contains port location
+require('dotenv').config()
+
+// Importing express and setting it to app variable
+const express = require('express')
+const app = express()
+
+// Importing routes and assiging them to variables
+const indexPath = require('./routes/index')
+const aboutPath = require('./routes/about')
+const contactPath = require('./routes/contact')
+
+// Consuming route variables
+app.use(indexPath)
+app.use(aboutPath)
+app.use(contactPath)
+
+// Setting public folder for static content (Images, CSS, and JS)
+// Note: This is the compiled CSS. I bypassed express and use npm scripts
+// To use SCSS.
+app.use('/static', express.static('public'))
+
+// Setting listening port from the env file
+app.listen(process.env.PORT)
